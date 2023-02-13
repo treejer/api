c67eb2f98fbf9db5d11fa1671c579d123302ecdf
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2023-02-13 22:59:34 +0330

    add error msg

--- src/offchainPlanting/assignedTreePlant.service.ts
+++ src/offchainPlanting/assignedTreePlant.service.ts
@@ -37,21 +37,15 @@ export class AssignedTreePlantService {
     private updateTreeRepository: UpdateTreeRepository,
     private assignedTreePlantRepository: AssignedTreePlantRepository,
     private treePlantRepository: TreePlantRepository,
-    private userService: UserService,
+    private userService: UserService
   ) {}
 
   async updateTree(dto: UpdateTreeDto) {
     let tree = await getTreeData(dto.treeId);
-    console.log(
-      "dto.signer",
-
-      ethUtil.toChecksumAddress(dto.signer),
-    );
 
     let user = await this.userService.findUserByWallet(dto.signer);
-    console.log("user", user);
 
-    if (!user) return "not-found user";
+    if (!user) throw new ForbiddenException(AuthErrorMessages.USER_NOT_EXIST);
 
     const signer: string = await getSigner(
       dto.signature,
@@ -60,34 +54,40 @@ export class AssignedTreePlantService {
         treeId: dto.treeId,
         treeSpecs: dto.treeSpecs,
       },
-      3,
+      3
     );
 
     if (
       ethUtil.toChecksumAddress(signer) !==
       ethUtil.toChecksumAddress(dto.signer)
     )
-      return "invalid signer";
+      throw new BadRequestException(AuthErrorMessages.INVALID_SIGNER);
 
-    if (tree.treeStatus > 3) return "invalid-tree status";
+    if (tree.treeStatus > 3)
+      throw new ForbiddenException(
+        OffChainPlantingErrorMessage.INVALID_TREE_STATUS
+      );
 
     if (
       ethUtil.toChecksumAddress(tree.planter) !==
       ethUtil.toChecksumAddress(dto.signer)
     )
-      return "invalid planter";
+      throw new ForbiddenException(
+        OffChainPlantingErrorMessage.INVALID_PLANTER
+      );
 
     let now = Math.floor(new Date().getTime() / 1000);
 
     if (now < tree.plantDate + tree.treeStatus * 3600 + 604800)
-      return "early update";
+      throw new ForbiddenException(OffChainPlantingErrorMessage.EARLY_UPDATE);
 
     let pendingUpdates = await this.updateTreeRepository.findOne({
       status: 1,
       treeId: dto.treeId,
     });
 
-    console.log("sdssssss", pendingUpdates);
+    if (pendingUpdates)
+      throw new ForbiddenException(OffChainPlantingErrorMessage.PENDING_UPDATE);
 
     await this.updateTreeRepository.create({ ...dto });
   }
@@ -97,7 +97,7 @@ export class AssignedTreePlantService {
 
     let user = await this.userService.findUserByWallet(dto.signer);
 
-    if (!user) return "not-found user";
+    if (!user) throw new ForbiddenException(AuthErrorMessages.USER_NOT_EXIST);
 
     const signer = await getSigner(
       dto.signature,
@@ -108,17 +108,20 @@ export class AssignedTreePlantService {
         birthDate: dto.birthDate,
         countryCode: dto.countryCode,
       },
-      1,
+      1
     );
 
     if (
       ethUtil.toChecksumAddress(signer) !==
       ethUtil.toChecksumAddress(dto.signer)
     )
-      return "invalid signer";
+      throw new BadRequestException(AuthErrorMessages.INVALID_SIGNER);
 
-    if (tree.treeStatus != 2) return "invalid-tree status";
-    console.log("tre", tree);
+    if (tree.treeStatus != 2)
+      throw new ForbiddenException(
+        OffChainPlantingErrorMessage.INVALID_TREE_STATUS
+      );
+
     let pendingPlants = await this.getSignedMessagesList({
       signer: dto.signer,
       status: 0,
@@ -126,28 +129,31 @@ export class AssignedTreePlantService {
 
     const planterData = await getPlanterData(signer);
 
-    console.log("planterDa", planterData);
-
-    if (planterData.status != 1) return "invalid planter";
-
-    console.log("signer", signer);
-    console.log("tree.planter", tree.planter);
+    if (planterData.status != 1)
+      throw new ForbiddenException(
+        OffChainPlantingErrorMessage.INVALID_PLANTER
+      );
 
     if (
       ethUtil.toChecksumAddress(tree.planter) !==
       ethUtil.toChecksumAddress(signer)
     ) {
-      if (planterData.planterType != 3) return "invlid planter data";
-
-      if (tree.planter != (await getPlanterOrganization(signer)))
-        return "planter org invalid";
+      if (
+        !(
+          planterData.planterType == 3 &&
+          tree.planter == (await getPlanterOrganization(signer))
+        )
+      )
+        throw new ForbiddenException(
+          OffChainPlantingErrorMessage.INVALID_PLANTER
+        );
     }
 
     if (
       planterData.plantedCount + pendingPlants.length >=
       planterData.supplyCap
     )
-      return "supply error";
+      throw new ForbiddenException(OffChainPlantingErrorMessage.SUPPLY_ERROR);
 
     await this.userService.updateUserById(user._id, {
       plantingNonce: user.plantingNonce + 1,
@@ -180,7 +186,7 @@ export class AssignedTreePlantService {
         birthDate: dto.birthDate,
         countryCode: dto.countryCode,
       },
-      2,
+      2
     );
 
     if (
@@ -193,7 +199,7 @@ export class AssignedTreePlantService {
 
     if (planterData.status != 1)
       throw new ForbiddenException(
-        OffChainPlantingErrorMessage.INVALID_PLANTER,
+        OffChainPlantingErrorMessage.INVALID_PLANTER
       );
 
     let count: number = await this.pendingListCount({
