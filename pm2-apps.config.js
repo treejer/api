08a7663dde38e94c28f6f0d1db8c509c673866a0
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2023-03-05 17:09:59 +0330

    add pm2 in project

---
+++ pm2-apps.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "nest start --watch",
+      error_file: ".pm2/logs/app/app-error.log",
+      out_file: ".pm2/logs/app/app-out.log",
+      log_file: ".pm2/logs/app/app-log.log",
+    },
+    {
+      name: "listener",
+      script: "npx nestjs-command listener:run",
+      error_file: ".pm2/logs/listener/listener-error.log",
+      out_file: ".pm2/logs/listener/listener-out.log",
+      log_file: ".pm2/logs/listener/listener-log.log",
+    },
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,10 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup("api", app, document);
 
   await app.listen(3333);
 }
+
 bootstrap();
