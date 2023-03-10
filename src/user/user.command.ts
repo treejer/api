9a70b6419db0348b38ce7eb1f375ccc97778cd56
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2023-03-10 20:27:02 +0330

    add command

--- src/user/user.command.ts
+++ src/user/user.command.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { Command, Option } from "nestjs-command";
+import { Command, Positional } from "nestjs-command";
 import { Role } from "src/common/constants";
 
 @Injectable()
@@ -8,25 +8,23 @@ export class UserCommand {
   constructor(private readonly userService: UserService) {}
 
   @Command({
-    command: "add:role",
+    command: "add:role <wallet> <role>",
     describe: "add role for user",
   })
   async addRole(
-    @Option({
+    @Positional({
       name: "wallet",
       describe: "the wallet address",
       type: "string",
-      required: false,
     })
     wallet: string,
 
-    @Option({
+    @Positional({
       name: "role",
       describe: "the role",
       type: "number",
-      required: false,
     })
-    role: number,
+    role: Role
   ) {
     this.userService.updateRole(wallet, role);
   }
