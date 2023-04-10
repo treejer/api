5a587391ab97298a7b255f3005bfa263132680c3
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2023-04-10 19:17:50 +0330

    add swagger error document for admin and tree and etherValues

--- src/tree/tree.controller.ts
+++ src/tree/tree.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { TreeService } from "./tree.service";
 
 @ApiTags("trees")
@@ -7,6 +7,29 @@ import { TreeService } from "./tree.service";
 export class TreeController {
   constructor(private treeService: TreeService) {}
 
+  @ApiOperation({ summary: "get tree data" })
+  @ApiResponse({
+    status: 200,
+    description: "get tree data successfully.",
+  })
+  @ApiResponse({
+    status: 404,
+    description: "tree not found",
+    content: {
+      "text/plain": {
+        schema: { format: "text/plain", example: "Tree not Found" },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: "Response for Internal server error.",
+    content: {
+      "text/plain": {
+        schema: { format: "text/plain", example: "Internal Server Error" },
+      },
+    },
+  })
   @Get("/:id")
   getTree(@Param("id") id: string) {
     return this.treeService.getTree(id);
