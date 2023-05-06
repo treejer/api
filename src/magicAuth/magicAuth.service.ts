618f460475c02f34864280ad8ce7c8076e3aac90
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2023-05-06 21:24:37 +0330

    fix magic bug

--- src/magicAuth/magicAuth.service.ts
+++ src/magicAuth/magicAuth.service.ts
@@ -1,14 +1,25 @@
 import { Magic } from "@magic-sdk/admin";
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { checkClientMagicToken } from "src/common/helpers/magic";
 import { CreateMagicAuthDto, UserMetadataResultDto } from "./dto";
 import { MagicAuthRepository } from "./magicAuth.repository";
 
-const magic = new Magic(process.env.MAGIC_SECRET_API_KEY);
-
 @Injectable()
 export class MagicAuthService {
-  constructor(private magicAuthRepository: MagicAuthRepository) {}
+  private magic;
+  constructor(
+    private magicAuthRepository: MagicAuthRepository,
+    private configService: ConfigService
+  ) {
+    this.magic = new Magic(configService.get<string>("MAGIC_SECRET_API_KEY"));
+
+    console.log(
+      "magic",
+      configService.get<string>("MAGIC_SECRET_API_KEY")[22],
+      configService.get<string>("MAGIC_SECRET_API_KEY")[23]
+    );
+  }
 
   async createMagicAuth(magicAuth: CreateMagicAuthDto) {
     return await this.magicAuthRepository.create({ ...magicAuth });
@@ -16,6 +27,6 @@ export class MagicAuthService {
 
   async getUserMetaData(token: string): Promise<UserMetadataResultDto> {
     let tempToken = checkClientMagicToken(token);
-    return await magic.users.getMetadataByToken(tempToken);
+    return await this.magic.users.getMetadataByToken(tempToken);
   }
 }
