888cc258545b59b385cb016a99e7c3ba1a5a3be3
Mehdi Gorbanzadeh <M.Gorbanzadeh@parlar.com>
2023-06-10 11:52:53 +0330

    magic-auth

--- src/common/graphQuery/getSubmittedQuery.ts
+++ src/common/graphQuery/getSubmittedQuery.ts
@@ -2,9 +2,44 @@
 export const getSubmittedQuery = (planterAddress:string,skip:number,limit:number) => {
     return `{
         trees(skip:${skip},first:${limit},where: { planter:"${planterAddress.toLowerCase()}"}){
-          id
-          treeStatus
-          plantDate
-        }
+            id
+            treeStatus
+            countryCode
+            plantDate
+            birthDate
+            saleType
+            createdAt
+      
+            funder {
+              id
+            }
+      
+            treeSpecsEntity {
+                id
+                name
+                description
+                externalUrl
+                imageFs
+                imageHash
+                symbolFs
+                symbolHash
+                animationUrl
+                diameter
+                latitude
+                longitude
+                attributes
+                updates
+                nursery
+                locations
+            }
+      
+            lastUpdate  {
+              id
+              updateStatus
+              updateSpecs
+              createdAt
+              updatedAt
+            }
+      }
     }`
 };
